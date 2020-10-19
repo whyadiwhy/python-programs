893067232e50cfd2296ee74b1272e64b9774eba0
Aditya Gupta <adityagupta22081999@gmail.com>
2020-10-19 23:38:03 +0530

    Create sqaure.py

---
+++ sqaure.py
@@ -0,0 +1,8 @@
+rows = 5
+for i in range(1, rows + 1):
+    for j in range(1, rows + 1):
+        if j <= i:
+            print(i, end=' ')
+        else:
+            print(j, end=' ')
+    print()
